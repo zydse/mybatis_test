f728da8d3c625950e9ab2c57d795e9445a7d9bb0
zydse <z911923564@gmail.com>
2020-01-06 20:05:12 +0800

    实现基本的CRUD功能,增加log4j的配置文件

--- src/main/java/top/zydse/dao/IUserDao.java
+++ src/main/java/top/zydse/dao/IUserDao.java
@@ -11,5 +11,48 @@ import java.util.List;
  * @Date: 2020/1/3
  */
 public interface IUserDao {
+    /**
+     * 查询所有用户信息
+     * @return
+     */
     List<User> findAll();
+
+    /**
+     * 保存一个用户信息到数据库中
+     * @param user
+     */
+    void saveUser(User user);
+
+    /**
+     * 更新一个用户的信息
+     * @param user
+     */
+    void updateUser(User user);
+
+    /**
+     * 根据用户id删除一个记录
+     * @param userId
+     */
+    void deleteUser(Integer userId);
+
+    /**
+     * 根据ID查询一条记录
+     * @param userId
+     * @return
+     */
+    User findById(Integer userId);
+
+    /**
+     * 根据模糊字符串查询所有匹配的记录
+     * ！important 传入的参数要带有%%
+     * @param uname
+     * @return
+     */
+    List<User> findByName(String uname);
+
+    /**
+     * 查询记录总条数
+     * @return
+     */
+    int findTotal();
 }
--- src/test/java/top/zydse/dao/IUserDaoTest.java
+++ src/test/java/top/zydse/dao/IUserDaoTest.java
@@ -4,10 +4,13 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import top.zydse.domain.User;
 
 import java.io.InputStream;
+import java.util.Date;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -20,24 +23,86 @@ import static org.junit.Assert.*;
  */
 public class IUserDaoTest {
 
+    private InputStream in;
+    private SqlSession session;
+    private IUserDao userDao;
+
+    @Before
+    public void init() throws Exception {
+        //读取配置文件
+        in = Resources.getResourceAsStream("SqlMapConfig.xml");
+        //创建SqlSessionFactory工厂
+        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(in);
+        //使用工厂生产SqlSession对象
+        session = factory.openSession();
+        //使用SqlSession创建Mapper接口的动态代理对象
+        userDao = session.getMapper(IUserDao.class);
+    }
+
+    @After
+    public void destroy() throws Exception {
+        session.commit();
+
+        session.close();
+        in.close();
+    }
+
     @Test
     public void findAll() throws Exception{
-        //读取配置文件
-        InputStream in = Resources.getResourceAsStream("SqlMapConfig.xml");
-        //创建SqlSessionFactory工厂
-        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
-        SqlSessionFactory factory = builder.build(in);
-        //使用工厂生产SqlSession对象
-        SqlSession session = factory.openSession();
-        //使用SqlSession创建Mapper接口的动态代理对象
-        IUserDao userDao = session.getMapper(IUserDao.class);
+
         //使用代理对象执行方法
         List<User> users = userDao.findAll();
         for (User user : users) {
             System.out.println(user);
         }
-        //释放资源
-        session.close();
-        in.close();
+
+    }
+
+    @Test
+    public void saveUser() throws  Exception {
+        User user = new User();
+        user.setUsername("Tom");
+        user.setSex("男");
+        user.setAddress("江苏南京");
+        user.setBirthday(new Date());
+
+        userDao.saveUser(user);
+    }
+
+    @Test
+    public void updateUser(){
+        User user = new User();
+        user.setId(8);
+        user.setUsername("阿曼达");
+        user.setSex("女");
+        user.setAddress("江苏南京");
+        user.setBirthday(new Date());
+
+        userDao.updateUser(user);
+    }
+
+    @Test
+    public void deleteUser(){
+        userDao.deleteUser(4);
+    }
+
+    @Test
+    public void findById(){
+        User user = userDao.findById(3);
+        System.out.println(user);
+    }
+
+    @Test
+    public void findByName(){
+        List<User> users = userDao.findByName("%王%");
+        for (User user : users){
+            System.out.println(user);
+        }
+    }
+
+    @Test
+    public void findTotal(){
+        int count = userDao.findTotal();
+        System.out.println(count);
     }
 }
